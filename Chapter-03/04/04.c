313abf2ecdffde070cefe3c59596d4bdf822f1d8
Andrew <apchajek1@gmail.com>
2025-03-22 00:02:07 -0500

    feat: add project solutions 04-06 for chapter-03

---
+++ Chapter-03/04/04.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int part1, part2, part3;
+
+    printf("Enter phone number [(xxx) xxx-xxxx]: ");
+    scanf(" (%d) %d-%d", &part1, &part2, &part3);
+    printf("You entered %.3d.%.3d.%.4d\n", part1, part2, part3);
+
+    return 0;
+}
