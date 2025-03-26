2886eb769f4f8d6744e4ca0b63c6bd58b3fa5ea2
Andrew <apchajek1@gmail.com>
2025-03-25 23:28:31 -0500

    feat: add project solutions 01-06 for chapter-06

---
+++ Chapter-06/03/03.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    int numerator, denominator, n, m, remainder;
+
+    printf("Enter a fraction: ");
+    scanf("%d / %d", &numerator, &denominator);
+
+    n = numerator;
+    m = denominator;
+
+    while (n != 0) {
+        remainder = m % n;
+        m = n;
+        n = remainder;
+    }
+
+    printf("In lowest terms: %d/%d\n", numerator / m, denominator / m);
+    return 0;
+}
